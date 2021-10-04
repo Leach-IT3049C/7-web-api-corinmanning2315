159bcb578984d4b46ac65d5e6f8f2b5c6eb451b8
corinmanning2315 <manninc2@mail.uc.edu>
2021-10-03 22:49:46 -0400

    finished product

---
+++ resources/js/index.js
@@ -0,0 +1,28 @@
+navigator.getBattery().then(function(battery) {
+    function updateAllBatteryInfo(){
+      updateChargeInfo();
+      updateLevelInfo();
+    }
+    updateAllBatteryInfo();
+ 
+    battery.addEventListener('chargingchange', function(){
+      updateChargeInfo();
+    });
+    function updateChargeInfo(){
+      console.log("Battery charging? "
+                  + (battery.charging ? "Yes" : "No"));
+    }
+ 
+    battery.addEventListener('levelchange', function(){
+      updateLevelInfo();
+    });
+    function updateLevelInfo(){
+      console.log("Battery level: "
+                  + battery.level * 100 + "%");
+                  var status = battery.level * 100 +"%";
+                  document.getElementById("batteryLevel").style.width = status;
+                  document.getElementById("percent").innerText = status;
+
+
+    }  
+  });
